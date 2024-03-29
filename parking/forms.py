c148cd953d44921a7c391de642f25e1893f0dcdf
mvarbichki <116360901+mvarbichki@users.noreply.github.com>
2024-03-29 14:48:31 +0200

    Improving forms - moving messages in separate module

--- parking/forms.py
+++ parking/forms.py
@@ -7,6 +7,10 @@ from parking.utilities import blank_space_check, validate_check_from_lst, num_pl
     srt_reg_num_cyrillic_compare_lst, str_color_latin_dash_compare_lst, str_color_cyrillic_dash_compare_lst, \
     str_model_latin_compare_lst, str_model_cyrillic_compare_lst, first_last_ele_check, dt_format_db, dt_now, \
     calculate_days_diff, validate_datetime_differance
+from parking.messages import blank_space_msg, invalid_phone_msg, invalid_code_phone_msg, invalid_first_name_msg, \
+    invalid_last_name_msg, first_last_name_differance_msg, invalid_address_msg, address_name_differance_msg, \
+    invalid_reg_number_msg, invalid_veh_type_msg, invalid_brand_msg, brand_type_differance_msg, invalid_model_msg, \
+    model_type_differance_msg, invalid_color_msg, color_type_differance_msg, invalid_type_msg
 
 
 class LoginForm(FlaskForm):
@@ -26,53 +30,46 @@ class CustomersRegisterForm(FlaskForm):
             pass
         # checks of there is blank space
         elif blank_space_check(phone_to_check):
-            raise ValidationError("Remove any blank space from the phone number")
+            raise ValidationError(blank_space_msg("the phone number"))
         # checks if phone number is valid by comparing it to numb list and checks if + is not at first place
         elif (validate_check_from_lst(phone_to_check.data, num_plus_to_compare_lst)) or \
                 ([i for i in phone_to_check.data[1:] if i == "+"]):
-            raise ValidationError("The phone number is invalid. Only numbers and plus symbol are allowed")
+            raise ValidationError(invalid_phone_msg)
         # checks if phone number begin is valid, must start with +
         elif phone_to_check.data[0] != "+":
-            raise ValidationError("The phone number must begin with country code ex. +359, +90, +30 etc")
+            raise ValidationError(invalid_code_phone_msg)
 
     def validate_first_name(self, first_name_to_check):
         if blank_space_check(first_name_to_check):
-            raise ValidationError("Remove any blank space from the first name")
+            raise ValidationError(blank_space_msg("the first name"))
         # checks if the entry is only letters and if it mixes Latin and Cyrillic
         elif (validate_check_from_lst(first_name_to_check.data, latin_letters)) and \
                 (validate_check_from_lst(first_name_to_check.data, cyrillic_letters)):
-            raise ValidationError("The first name is invalid. Only letters are allowed."
-                                  " A mix between Latin and cyrillic letters is not allowed")
+            raise ValidationError(invalid_first_name_msg)
 
     def validate_last_name(self, last_name_to_check):
         if blank_space_check(last_name_to_check):
-            raise ValidationError("Remove any blank space from the last name")
+            raise ValidationError(blank_space_msg("the last name"))
         elif (validate_check_from_lst(last_name_to_check.data, latin_letters)) and \
                 (validate_check_from_lst(last_name_to_check.data, cyrillic_letters)):
-            raise ValidationError(
-                "The last name is invalid. Only letters are allowed."
-                " A mix between Latin and cyrillic letters is not allowed")
+            raise ValidationError(invalid_last_name_msg)
         # checks if the last name is different alphabet than the data
         elif (validate_check_from_lst(self.first_name.data, latin_letters) and
               validate_check_from_lst(last_name_to_check.data, cyrillic_letters)) or \
                 (validate_check_from_lst(self.first_name.data, cyrillic_letters) and
                  validate_check_from_lst(last_name_to_check.data, latin_letters)):
-            raise ValidationError("The first name and the last name are not the same type."
-                                  " Use latin only or cyrillic only for the customer's fields.")
+            raise ValidationError(first_last_name_differance_msg)
 
     def validate_address(self, address_to_check):
         if address_to_check.data:
             if (validate_check_from_lst(address_to_check.data, str_address_latin_compare_lst)) and \
                     (validate_check_from_lst(address_to_check.data, str_address_cyrillic_compare_lst)):
-                raise ValidationError(
-                    "The address is invalid. Only letters, numbers, apostrophe, dash, dot, comma are allowed."
-                    " A mix between latin and cyrillic letters is not allowed")
+                raise ValidationError(invalid_address_msg)
             elif (validate_check_from_lst(self.first_name.data, latin_letters) and
                   validate_check_from_lst(address_to_check.data, str_address_cyrillic_compare_lst)) or \
                     (validate_check_from_lst(self.first_name.data, cyrillic_letters) and
                      validate_check_from_lst(address_to_check.data, str_address_latin_compare_lst)):
-                raise ValidationError("The address and customer name are not the same type."
-                                      " Use latin only or cyrillic only for the customer's fields")
+                raise ValidationError(address_name_differance_msg)
 
     first_name = StringField(label="First name: *",
                              validators=[InputRequired(), Length(min=2, max=20)]
@@ -110,69 +107,56 @@ class VehiclesRegisterForm(FlaskForm):
         if reg_number_to_check.data == "":
             pass
         elif blank_space_check(reg_number_to_check):
-            raise ValidationError("Remove any blank space from the registration number")
+            raise ValidationError(blank_space_msg("the registration number"))
         # checks for allowed characters and symbols
         elif validate_check_from_lst(reg_number_to_check.data, str_reg_num_latin_compare_lst) and \
                 validate_check_from_lst(reg_number_to_check.data, srt_reg_num_cyrillic_compare_lst) or \
                 (first_last_ele_check(reg_number_to_check, "-")):
-            raise ValidationError(
-                "The vehicle registration number is invalid. Only letters, numbers and dash symbol (must not be "
-                "first or last) are allowed. A mix between latin and cyrillic letters is not allowed")
+            raise ValidationError(invalid_reg_number_msg)
 
     def validate_vehicle_type(self, type_to_check):
         if blank_space_check(type_to_check):
-            raise ValidationError("Remove any blank space from the vehicle type")
+            raise ValidationError(blank_space_msg("the vehicle type"))
         elif (validate_check_from_lst(type_to_check.data, latin_letters)) and \
                 (validate_check_from_lst(type_to_check.data, cyrillic_letters)):
-            raise ValidationError(
-                "The vehicle type is invalid. Only letters are allowed."
-                " A mix between latin and cyrillic letters is not allowed")
+            raise ValidationError(invalid_veh_type_msg)
 
     def validate_brand(self, brand_to_check):
         if blank_space_check(brand_to_check):
-            raise ValidationError("Remove any blank space from the vehicle brand")
+            raise ValidationError(blank_space_msg("the vehicle brand"))
         elif (validate_check_from_lst(brand_to_check.data, latin_letters)) and \
                 (validate_check_from_lst(brand_to_check.data, cyrillic_letters)):
-            raise ValidationError(
-                "The vehicle brand is invalid. Only letters are allowed."
-                " A mix between latin and cyrillic letters is not allowed")
+            raise ValidationError(invalid_brand_msg)
         elif (validate_check_from_lst(self.vehicle_type.data, latin_letters) and
               validate_check_from_lst(brand_to_check.data, cyrillic_letters)) or \
                 (validate_check_from_lst(self.vehicle_type.data, cyrillic_letters) and
                  validate_check_from_lst(brand_to_check.data, latin_letters)):
-            raise ValidationError("The vehicle brand and vehicle type are not the same type."
-                                  " Use latin only or cyrillic only for the vehicle's fields")
+            raise ValidationError(brand_type_differance_msg)
 
     def validate_model(self, model_to_check):
         if blank_space_check(model_to_check):
-            raise ValidationError("Remove any blank space from the vehicle model")
+            raise ValidationError(blank_space_msg("the vehicle model"))
         elif (validate_check_from_lst(model_to_check.data, str_model_latin_compare_lst)) and \
                 (validate_check_from_lst(model_to_check.data, str_model_cyrillic_compare_lst)):
-            raise ValidationError(
-                "The vehicle model is invalid. Only letters are allowed."
-                " A mix between latin and cyrillic letters is not allowed")
+            raise ValidationError(invalid_model_msg)
         elif (validate_check_from_lst(self.vehicle_type.data, str_reg_num_latin_compare_lst) and
               validate_check_from_lst(model_to_check.data, str_model_cyrillic_compare_lst)) or \
                 (validate_check_from_lst(self.vehicle_type.data, srt_reg_num_cyrillic_compare_lst) and
                  validate_check_from_lst(model_to_check.data, str_model_latin_compare_lst)):
-            raise ValidationError("The vehicle model and vehicle type are not the same type."
-                                  " Use latin only or cyrillic only for the vehicle's fields")
+            raise ValidationError(model_type_differance_msg)
 
     def validate_color(self, color_to_check):
         if blank_space_check(color_to_check):
-            raise ValidationError("Remove any blank space from the vehicle color")
+            raise ValidationError(blank_space_msg("the vehicle color"))
         elif (validate_check_from_lst(color_to_check.data, str_color_latin_dash_compare_lst)) and \
                 (validate_check_from_lst(color_to_check.data, str_color_cyrillic_dash_compare_lst)) or \
                 (first_last_ele_check(color_to_check, "-")):
-            raise ValidationError(
-                "The vehicle color is invalid. Only letters and dash symbol (must not be "
-                "first or last) are allowed. A mix between latin and cyrillic letters is not allowed")
+            raise ValidationError(invalid_color_msg)
         elif (validate_check_from_lst(self.vehicle_type.data, str_reg_num_latin_compare_lst) and
               validate_check_from_lst(color_to_check.data, str_color_cyrillic_dash_compare_lst)) or \
                 (validate_check_from_lst(self.vehicle_type.data, srt_reg_num_cyrillic_compare_lst) and
                  validate_check_from_lst(color_to_check.data, str_color_latin_dash_compare_lst)):
-            raise ValidationError("The vehicle color and vehicle type are not the same type."
-                                  " Use latin only or cyrillic only for the vehicle's fields")
+            raise ValidationError(color_type_differance_msg)
 
     vehicle_registration_number = StringField(label="Vehicle registration number: *",
                                               validators=[InputRequired(), Length(min=7, max=20)]
@@ -196,12 +180,10 @@ class EditVehicleFrom(VehiclesRegisterForm):
 
     def validate_vehicle_type(self, type_to_check):
         if blank_space_check(type_to_check):
-            raise ValidationError("Remove any blank space from the vehicle type")
+            raise ValidationError(blank_space_msg("the vehicle type"))
         elif (validate_check_from_lst(type_to_check.data, latin_letters)) and \
                 (validate_check_from_lst(type_to_check.data, cyrillic_letters)):
-            raise ValidationError(
-                "The vehicle type is invalid. Only letters are allowed."
-                " A mix between latin and cyrillic letters is not allowed")
+            raise ValidationError(invalid_type_msg)
 
 
 class AddSubscriptionForm(FlaskForm):
--- parking/messages.py
+++ parking/messages.py
@@ -6,6 +6,36 @@ update_record_msg = "The record was changed successfully"
 failed_update_record_msg = "Changing the record failed. Try again"
 successful_del_record_msg = "The record was deleted successfully"
 failed_del_record_msg = "Deleting the record failed. Try again"
+invalid_phone_msg = "The phone number is invalid. Only numbers and plus symbol are allowed"
+invalid_code_phone_msg = "The phone number must begin with country code ex. +359, +90, +30 etc"
+invalid_first_name_msg = ("The first name is invalid. Only letters are allowed.A mix between Latin and cyrillic "
+                          "letters is not allowed")
+invalid_last_name_msg = ("The last name is invalid. Only letters are allowed. A mix between Latin and cyrillic letters "
+                         "is not allowed")
+first_last_name_differance_msg = ("The first name and the last name are not the same type.Use latin only or cyrillic "
+                                  "only for the customer's fields.")
+invalid_address_msg = ("The address is invalid. Only letters, numbers, apostrophe, dash, dot, comma are allowed. A mix "
+                       "between latin and cyrillic letters is not allowed")
+address_name_differance_msg = ("The address and customer name are not the same type. Use latin only or cyrillic only "
+                               "for the customer's fields")
+invalid_reg_number_msg = ("The vehicle registration number is invalid. Only letters, numbers and dash symbol (must not "
+                          "be first or last) are allowed. A mix between latin and cyrillic letters is not allowed")
+invalid_veh_type_msg = ("The vehicle type is invalid. Only letters are allowed. A mix between latin and cyrillic "
+                        "letters is not allowed")
+invalid_brand_msg = ("The vehicle brand is invalid. Only letters are allowed.A mix between latin and cyrillic letters "
+                     "is not allowed")
+brand_type_differance_msg = ("The vehicle brand and vehicle type are not the same type. Use latin only or cyrillic "
+                             "only for the vehicle's fields")
+invalid_model_msg = ("The vehicle model is invalid. Only letters are allowed. A mix between latin and cyrillic letters "
+                     "is not allowed")
+model_type_differance_msg = ("The vehicle model and vehicle type are not the same type. Use latin only or cyrillic "
+                             "only for the vehicle's fields")
+invalid_color_msg = ("The vehicle color is invalid. Only letters and dash symbol (must not be first or last) are "
+                     "allowed. A mix between latin and cyrillic letters is not allowed")
+color_type_differance_msg = ("The vehicle color and vehicle type are not the same type. Use latin only or cyrillic "
+                             "only for the vehicle's fields")
+invalid_type_msg = ("The vehicle type is invalid. Only letters are allowed. A mix between latin and cyrillic letters "
+                    "is not allowed")
 
 
 def record_exist_msg(record):
@@ -34,3 +64,7 @@ def overdue_fee(obj):
 
 def login_msg(obj):
     return f"{obj} logged in"
+
+
+def blank_space_msg(obj):
+    return f"Remove any blank space from {obj}"
